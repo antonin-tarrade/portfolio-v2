f3450051f1ced1335e4cdff86bccab0af67dd5e7
antonin-tarrade <antotarrade@gmail.com>
2025-04-05 10:01:43 +0200

    DNS check + Init WIP status

--- eportfolio-app/src/App.js
+++ eportfolio-app/src/App.js
@@ -5,17 +5,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Work In Progress</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Site under construction
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://antonin-tarrade.github.io"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit my previous site
         </a>
       </header>
     </div>
